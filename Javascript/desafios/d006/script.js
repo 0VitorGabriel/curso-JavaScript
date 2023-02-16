2e3380e968f44c870b4cb5340550028aeeb5ed6c
Vitor Gabriel <victoropo64@outlook.com>
2023-02-16 19:20:17 -0400

    mofied d006

--- Javascript/desafios/d006/script.js
+++ Javascript/desafios/d006/script.js
@@ -1,18 +1,16 @@
 function conversor() {
     var temperatura = document.getElementById('temperatura')
-    var res_fahrenheit = document.getElementById('fahrenheit')
-    var res_kelvin = document.getElementById('kelvin')
-    var graus_padrao_texto = prompt('Digite uma temperatura em C (Celsius)')
+    var resultado = document.getElementById('resultado')
+    var graus_padrao = Number(prompt('Digite uma temperatura em C (Celsius)'))
 
-    var graus_padrao = Number(graus_padrao_texto)
-    var fahrenheit = (graus_padrao * 9 / 5) + 32
-    var kelvin = graus_padrao + 273.15
-
-    if (graus_padrao_texto.length == 0) {
+    if (graus_padrao.length == 0) {
         alert('ERRO, Preencha os campos para continuar!')
     } else { 
-        temperatura.innerText = `A temperatura ${graus_padrao} C, corresponde a...`
-        res_fahrenheit.innerText = `${fahrenheit} F (Fahrenheit)`
-        res_kelvin.innerText = `${kelvin} K (Kelvin)`
+        var fahrenheit = (graus_padrao * 9 / 5) + 32
+        var kelvin = graus_padrao + 273.15
+
+        resultado.innerHTML = `<h1>A temperatura ${graus_padrao} C, corresponde a...</h1>`
+        resultado.innerHTML += `<p>${fahrenheit} F (Fahrenheit)</p>`
+        resultado.innerHTML += `<p>${kelvin} K (Kelvin)</p>`
     }
 }
