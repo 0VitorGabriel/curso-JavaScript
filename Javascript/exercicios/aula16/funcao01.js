de6403d06ab47b37bd025cbcd7dc286459ee8f77
Vitor Gabriel <victoropo64@outlook.com>
2023-02-24 09:47:39 -0400

    finsihed class

---
+++ Javascript/exercicios/aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimpar(numero) {
+    if (numero % 2 === 0) {
+        return 'Número par'
+    } else {
+        return 'Número impar'
+    }
+}
+
+console.log(parimpar(10))
---
+++ Javascript/exercicios/aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(pri_num = 0, seg_num = 0) {
+    return pri_num + seg_num
+}
+
+console.log(soma(1))
