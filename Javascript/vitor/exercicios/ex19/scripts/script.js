5c30ff5ecbc827dc1ccfa2ea1e8ed749e245116b
Vitor Gabriel <victoropo64@outlook.com>
2023-02-28 19:36:13 -0400

    terminei exercicio 19

--- Javascript/vitor/exercicios/ex19/scripts/script.js
+++ Javascript/vitor/exercicios/ex19/scripts/script.js
@@ -34,3 +34,33 @@ function adicionar() {
     txt_numero.value = ''
     txt_numero.focus()
 }
+11
+function finalizar() {
+    let total_numero = lista.length
+    let maior = lista[0]
+    let menor = lista[0]
+    let soma = 0
+
+    for (let pos in lista) {
+        soma += lista[pos]
+
+        if (lista[pos] > maior) {
+            maior = lista[pos]
+        } if (lista[pos] < menor) {
+            menor = lista[pos]
+        }
+    }
+    
+    let media = soma / total_numero
+
+    resposta.innerHTML = `<p> ${maior} ${menor} ${soma} ${media} ${total_numero} </p>`
+    mostrar_resultado(maior, menor, soma, media, total_numero)
+}
+
+function mostrar_resultado(mai, men, som, med, tot) {
+    resposta.innerHTML = `<p>Ao todo foram ${tot} números adicionados</p>`
+    resposta.innerHTML += `<p>Maior número digitado: ${mai}</p>`
+    resposta.innerHTML += `<p>Menor número digitado: ${men}</p>`
+    resposta.innerHTML += `<p>Soma de todos os números: ${som}</p>`
+    resposta.innerHTML += `<p>Média dos números: ${med.toFixed(2)}</p>`
+}
