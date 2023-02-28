40aa3ab2528318dff76e3164f23f6ad1ed0553fd
Vitor Gabriel <victoropo64@outlook.com>
2023-02-28 09:33:00 -0400

    Update script.js

--- Javascript/vitor/exercicios/ex17/script.js
+++ Javascript/vitor/exercicios/ex17/script.js
@@ -4,33 +4,36 @@ const txt_passo = document.querySelector('input#passo')
 let resultado = document.querySelector('div#resultado')
 
 function verificador() {
-    if (inicio.value.length === 0 || fim.value.length == 0 || passo.value.length === 0) {
+    if (inicio.value.length === 0 || fim.value.length === 0 || passo.value.length === 0) {
         alert('[ERRO] faltam dados!')
         resultado.innerHTML = 'Impossivel contar...'
+        return true
     }
 }
 
 function contar() {
-    verificador()
+    if (verificador()) {
+        // executa ele mesmo
+    } else {
+        resultado.innerHTML = 'Contando... <br>'
 
-    resultado.innerHTML = 'Contando... <br>'
+        let inicio = Number(txt_inicio.value)
+        let fim = Number(txt_fim.value)
+        let passo = Number(txt_passo.value)
 
-    let inicio = Number(txt_inicio.value)
-    let fim = Number(txt_fim.value)
-    let passo = Number(txt_passo.value)
+        if (passo <= 0) {
+            alert('Passo inválido, considernado passo 1')
+            passo = 1
+        }
 
-    if (passo <= 0) {
-        alert('Passo inválido, considernado passo 1')
-        passo = 1
+        if (inicio < fim) {
+            contagem_progressiva(inicio, fim, passo) 
+        } else if(inicio > fim) {
+            contagem_regressiva(inicio, fim, passo)
+        }
+
+        resultado.innerHTML +=  ` \u{1F3C1}`
     }
-
-    if (inicio < fim) {
-        contagem_progressiva(inicio, fim, passo) 
-    } else if(inicio > fim) {
-        contagem_regressiva(inicio, fim, passo)
-    }
-
-    resultado.innerHTML =  ` \u{1F3C1}`
 }
 
 function contagem_progressiva(ini, fi, pass) {
