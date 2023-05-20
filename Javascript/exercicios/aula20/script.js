257ab3182b6d644a1589aab776fe9609c45b9bc3
0VitorGabriel <victoropo64@outlook.com>
2023-05-19 21:22:32 -0400

    aula finalizad (Promise.all() e Promise.race())

--- Javascript/exercicios/aula20/script.js
+++ Javascript/exercicios/aula20/script.js
@@ -1,13 +1,17 @@
-const promise = Promise.resolve(3)
-const promise2 = Promise.resolve(4)
-const promise3 = Promise.resolve(5)
+const promise = Promise.resolve('primeiro valor')
+const promise2 = Promise.resolve('segundo valor')
+const promise3 = Promise.resolve('terceiro valor')
+
+// captura os dados de todas as promises
 
 Promise.all([promise, promise2, promise3])
     .then((values) => {
-
+        console.log(values)
     })
+    
+// captura o dado da primeira promise que achar um valor
 
 Promise.race([promise, promise2, promise3])
     .then((values) => {
-
+        console.log(values)
     })
